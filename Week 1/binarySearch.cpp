838e77f581b40c32f22e8928dd8f15c7b5fffc2a
saumya singh <saumya.konan@gmail.com>
2021-07-21 12:42:35 +0530

    Add files via upload

---
+++ Week 1/binarySearch.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+string linearSearch(int arr[],int n, int key)
+{
+    int l=0,u=n-1,c=0;
+    while(l<=u)
+    {
+        c++;
+        int mid=(l+u)/2;
+        if(arr[mid]==key)
+        {
+            return "Present "+to_string(c);
+        }
+        else if(arr[mid]>key)
+        {
+            u=mid-1;
+        }
+        else
+        {
+            l=mid+1;
+        }
+    }
+    return "Not Present"+to_string(c);
+}
+
+int main()
+{
+    int t,i,n,j,key;
+    cin>>t;
+    string ans[t];
+    for(i=0;i<t;i++)
+    {
+        cin>>n;
+        int arr[n];
+        for(j=0;j<n;j++)
+        {
+            cin>>arr[j];
+        }
+        cin>>key;
+        ans[i]=linearSearch(arr,n,key);
+    }
+    for(i=0;i<t;i++)
+    {
+        cout<<ans[i]<<endl;
+    }
+    return 0;
+}
